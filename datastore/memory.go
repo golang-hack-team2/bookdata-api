efcc86ecfd4067d31b2a8fc5e699d476681c6e46
Richard Cheney <richeney@microsoft.com>
2020-04-28 00:02:51 +0100

    Added in elapsed time using defer magic

--- datastore/memory.go
+++ datastore/memory.go
@@ -1,8 +1,16 @@
 package datastore
 
 import (
+	"log"
+	"time"
+
 	"github.com/matt-FFFFFF/bookdata-api/loader"
 )
+
+func elapsed(start time.Time, name string) {
+	elapsed := time.Since(start)
+	log.Printf("%s took %s", name, elapsed)
+}
 
 // Books is the memory-backed datastore used by the API
 // It contains a single field 'Store', which is (a pointer to) a slice of loader.BookData struct pointers
@@ -15,6 +23,7 @@ type Books struct {
 // You need to change this to load data from the CSV file
 func (b *Books) Initialize() {
 	//b.Store = &loader.BooksLiteral
+	defer elapsed(time.Now(), "Loading from CSV file")
 	booksList := loader.CsvReader()
 	b.Store = &booksList
 }
